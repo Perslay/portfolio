1737c07a01f7c13ac7d5b086d707ba4c0c830984
Natalia Fedyk <natalia.44.fedyk@gmail.com>
2025-02-27 17:05:33 +0100

    Change basename

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { App } from "./components/App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/portfolio">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </StrictMode>
