8d5219b7c11ab89ec0a97f763bc0ac5721f1ee8c
Perslay <natalia.44.fedyk@gmail.com>
2026-06-19 16:06:03 +0200

    Basic update of main page
    Changing role and description. A few styling changes.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -11,19 +11,21 @@ const Home = () => {
           srcSet="/images/avatar.jpg 1x, /images/avatar@2x.jpg 2x"
           alt="Young woman with fair skin and long mousy hair."
         />
-        <h2 className={css.secondHeading}>Web Development Aspirant</h2>
+        <h2 className={css.secondHeading}>Games Tester</h2>
         <p className={css.paragraph}>
-          My name is Natalia Fedyk and I create websites. Becoming a web
-          developer is my dream.
+          I have experience in testing games, reporting bugs and providing
+          feedback to developers. My attention to detail and ability to
+          recognize patterns make me a valuable asset to any quality assurance
+          team.
         </p>
       </section>
       <section className={css.section}>
         <h3 className={css.thirdHeading}>Curriculum Vitae</h3>
-        <p className={css.paragraph}>
-          Are you an employer looking for a Frontend or Fullstack Developer? You
-          can click one of the buttons below to see or download my CV.
-        </p>
-        <div className={css.cvLinks}>
+        <div className={css.cvDiv}>
+          <p className={css.paragraph}>
+            If you want to know more about my professional experience, skills
+            and education, you can view my CV by clicking on this link:
+          </p>
           <a
             className={css.cvLink}
             // href="/natalia_fedyk_cv.pdf"
@@ -31,15 +33,10 @@ const Home = () => {
             target="_blank"
             rel="noopener noreferrer"
           >
-            See
-          </a>
-          <a
-            className={css.cvLink}
-            // href="/natalia_fedyk_cv.pdf"
-            href={`${import.meta.env.BASE_URL}natalia_fedyk_cv.pdf`}
-            download
-          >
-            Download
+            See my CV
+            <svg className={css.arrowIcon} width="45" height="45">
+              <use href="/images/icons.svg#icon-arrow" />
+            </svg>
           </a>
         </div>
       </section>
